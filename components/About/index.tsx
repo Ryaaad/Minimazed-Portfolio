e26fde5944e568f26ddf546f4dfc492b7777860b
Ryaaad <87044522+Ryaaad@users.noreply.github.com>
2023-06-05 04:24:52 +0100

    Route Logic

---
+++ components/About/index.tsx
@@ -0,0 +1,16 @@
+import { IoLocationSharp } from "react-icons/io5";
+import { HiMail } from "react-icons/hi";
+import { BsFillPhoneFill } from "react-icons/bs";
+import Title from "../shared/Title";
+
+
+const About = () => {
+    return ( 
+        <div  className="py-[95px] px-[20px] " >
+          <Title title="About Me" />
+       
+        </div>
+     );
+}
+ 
+export default About;
